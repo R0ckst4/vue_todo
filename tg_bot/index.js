6a072cac2d62d870b9626d8e108cc1fb55c9e0f4
R0ck5t4 <actuallyamarocksta@gmail.com>
2022-11-20 13:31:24 +0300

    Worked on my bot, added more commands (mostly for development ), and also added a massive, that contains tasks from bot

--- tg_bot/index.js
+++ tg_bot/index.js
@@ -3,6 +3,13 @@ const TelegramApi = require("node-telegram-bot-api");
 const token = "5792008606:AAE6Acx7xr1DIwB2LOYigRRE9as_0KzTbto";
 
 const bot = new TelegramApi(token, { polling: true });
+let botTodoList = [];
+
+bot.setMyCommands([
+  { command: "/start", description: "Приветствие" },
+  { command: "/showtodo", description: "Список задач" },
+  { command: "/deletelast", description: "Удалить последнюю задачу" },
+]);
 
 bot.on("message", (msg) => {
   const text = msg.text;
@@ -11,6 +18,41 @@ bot.on("message", (msg) => {
     bot.sendMessage(
       chatId,
       "Привет, отправь мне тудушку и я добавлю ее в твой тудулист"
+    );
+  }
+  if (text === "/showtodo") {
+    botTodoList.length
+      ? bot.sendMessage(
+          chatId,
+          `Вот список добавленных задач ${JSON.stringify(botTodoList)}`
+        )
+      : bot.sendMessage(chatId, "Нет задач");
+    console.log(botTodoList);
+  }
+  if (text === "/deletelast") {
+    botTodoList.length
+      ? bot.sendMessage(
+          chatId,
+          `Задача ${
+            botTodoList[botTodoList.length - 1].description
+          } была удалена`
+        )
+      : bot.sendMessage(chatId, "Нечего удалять");
+    botTodoList.pop();
+    console.log(botTodoList);
+  } else if (text !== "/start" && text !== "/showtodo") {
+    let textMessage = {
+      id: Date.now(),
+      description: text,
+      important: false,
+      isWork: false,
+      done: false,
+    };
+    botTodoList.push(textMessage);
+    console.log(botTodoList);
+    bot.sendMessage(
+      chatId,
+      `Сообщение '${text}' было добавлено в список задач`
     );
   }
 });
